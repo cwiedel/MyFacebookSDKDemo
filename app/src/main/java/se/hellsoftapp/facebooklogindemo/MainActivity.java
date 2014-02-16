6a4443eda6f40980b9903522709b77c022d757cd
Erik Hellman <hellman@spotify.com>
2014-02-16 19:52:50 +0100

    publish photo

--- app/src/main/java/se/hellsoftapp/facebooklogindemo/MainActivity.java
+++ app/src/main/java/se/hellsoftapp/facebooklogindemo/MainActivity.java
@@ -1,13 +1,18 @@
 package se.hellsoftapp.facebooklogindemo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,8 +26,16 @@ import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends FragmentActivity {
+    public static final String TAG = "FacebookLoginDemo";
+    private static final int CAPTURE_PHOTO_REQUEST_CODE = 1001;
+    private Uri mCapturedPhotoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +52,7 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -57,9 +70,49 @@ public class MainActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAPTURE_PHOTO_REQUEST_CODE && resultCode == RESULT_OK) {
+            Log.d(TAG, "Photo captured: " + mCapturedPhotoUri);
+            MyFacebookSyncService.startPostFacebookPhoto(this, mCapturedPhotoUri);
+        }
+    }
+
     public void onFacebookRefresh(View view) {
         MyFacebookSyncService.startActionUpdateFromWall(this);
     }
+
+    public void onPostPhotoClicked(View view) {
+        Session session = Session.getActiveSession();
+        if (session.isOpened()) {
+            session.requestNewPublishPermissions(
+                    new Session.NewPermissionsRequest(this,
+                            "publish_stream"));
+
+            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mCapturedPhotoUri = getOutputPhotoUri(this);
+
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedPhotoUri);
+
+            startActivityForResult(takePhotoIntent, CAPTURE_PHOTO_REQUEST_CODE);
+        }
+    }
+
+    private static Uri getOutputPhotoUri(Context context) {
+        File mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        StringBuilder stringBuilder = new StringBuilder();
+        return Uri.fromFile(new File(stringBuilder.append(mediaStorageDir.getPath())
+                .append(File.separator)
+                .append("IMG_")
+                .append(timeStamp)
+                .append(".jpg").toString()));
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
@@ -94,20 +147,23 @@ public class MainActivity extends FragmentActivity {
             // may not be triggered. Trigger it if it's open/closed.
             Session session = Session.getActiveSession();
             if (session != null &&
-                    (session.isOpened() || session.isClosed()) ) {
+                    (session.isOpened() || session.isClosed())) {
                 onSessionStateChange(session, session.getState(), null);
             }
 
             mUiHelper.onResume();
         }
 
-        private void onSessionStateChange(Session session, SessionState state, Exception e) {
+        private void onSessionStateChange(final Session session, SessionState state, Exception e) {
             View refreshButton = getActivity().findViewById(R.id.refresh_button);
-            if(session.isOpened()) {
+            View photoButton = getActivity().findViewById(R.id.post_photo_button);
+            if (session.isOpened()) {
                 Toast.makeText(getActivity(), "User logged in to Facebook.", Toast.LENGTH_SHORT).show();
                 refreshButton.setEnabled(true);
-            } else if(session.isClosed()) {
+                photoButton.setEnabled(true);
+            } else if (session.isClosed()) {
                 Toast.makeText(getActivity(), "User logged out from Facebook.", Toast.LENGTH_SHORT).show();
+                photoButton.setEnabled(false);
                 refreshButton.setEnabled(false);
                 MyFacebookSyncService.startActionUserLogout(getActivity());
             }
@@ -138,31 +194,36 @@ public class MainActivity extends FragmentActivity {
         }
 
         @Override
-        public void onActivityCreated(Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.facebook_login, container, false);
+            LoginButton loginButton = (LoginButton) rootView.findViewById(R.id.facebookLoginButton);
+            loginButton.setFragment(this);
+            loginButton.setReadPermissions("user_status", "user_friends",
+                    "friends_status", "read_stream");
 
             mListAdapter = new SimpleCursorAdapter(getActivity(),
                     R.layout.facebook_message,
                     null,
-                    new String[] {MyFacebookWall.Contract.FROM_NAME,
-                            MyFacebookWall.Contract.MESSAGE},
-                    new int[] {R.id.from_name, R.id.message}, 0);
-
-            getLoaderManager().initLoader(0, null, this);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.facebook_login, container, false);
-            LoginButton loginButton = (LoginButton) rootView.findViewById(R.id.facebookLoginButton);
-            loginButton.setFragment(this);
-            loginButton.setReadPermissions("user_status",
-                    "user_friends", "friends_status",
-                    "read_stream");
+                    new String[]{MyFacebookWall.Contract.FROM_NAME,
+                            MyFacebookWall.Contract.MESSAGE, MyFacebookWall.Contract.FROM_ID},
+                    new int[]{R.id.from_name, R.id.message, R.id.profile_picture}, 0);
+            mListAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+                @Override
+                public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                    if (columnIndex == cursor.getColumnIndex(MyFacebookWall.Contract.FROM_ID)) {
+                        ((ProfilePictureView) view).setProfileId(cursor.getString(columnIndex));
+                        return true;
+                    }
+                    return false;
+                }
+            });
 
             ListView facebookMessages = (ListView) rootView.findViewById(R.id.facebook_message_list);
             facebookMessages.setAdapter(mListAdapter);
+
+            getLoaderManager().initLoader(0, null, this);
+
             return rootView;
         }
 
@@ -170,8 +231,9 @@ public class MainActivity extends FragmentActivity {
         public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
             return new CursorLoader(getActivity(),
                     MyFacebookWall.Contract.FACEBOOK_WALL_URI,
-                    new String[] {MyFacebookWall.Contract.ID,
+                    new String[]{MyFacebookWall.Contract.ID,
                             MyFacebookWall.Contract.FROM_NAME,
+                            MyFacebookWall.Contract.FROM_ID,
                             MyFacebookWall.Contract.MESSAGE},
                     null, null, null);
         }
@@ -179,11 +241,12 @@ public class MainActivity extends FragmentActivity {
         @Override
         public void onLoadFinished(Loader<Cursor> objectLoader, Cursor cursor) {
             mListAdapter.swapCursor(cursor);
+            Log.d(TAG, "Loader finished: " + cursor.getCount());
         }
 
         @Override
         public void onLoaderReset(Loader<Cursor> objectLoader) {
-
+            Log.d(TAG, "Loader reset!");
         }
     }
 
--- app/src/main/java/se/hellsoftapp/facebooklogindemo/MyFacebookSyncService.java
+++ app/src/main/java/se/hellsoftapp/facebooklogindemo/MyFacebookSyncService.java
@@ -1,13 +1,18 @@
 package se.hellsoftapp.facebooklogindemo;
 
 import android.app.IntentService;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
@@ -16,6 +21,8 @@ import com.facebook.model.GraphObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -27,6 +34,7 @@ public class MyFacebookSyncService extends IntentService {
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_UPDATE_FROM_WALL = "se.hellsoft.facebooklogindemo.action.UPDATE_FROM_WALL";
     private static final String ACTION_USER_LOGOUT = "se.hellsoft.facebooklogindemo.action.USER_LOGOUT";
+    private static final String ACTION_POST_PHOTO = "se.hellsoft.facebooklogindemo.action.POST_PHOTO";
     private static final String FACEBOOK_PREFS = "facebook_settings";
     private static final String NEXT_SINCE_VALUE = "nextSinceValue";
 
@@ -54,8 +62,15 @@ public class MyFacebookSyncService extends IntentService {
         context.startService(intent);
     }
 
+    public static void startPostFacebookPhoto(Context context, Uri photoUri) {
+        Intent intent = new Intent(context, MyFacebookSyncService.class);
+        intent.setAction(ACTION_POST_PHOTO);
+        intent.setData(photoUri);
+        context.startService(intent);
+    }
+
     public MyFacebookSyncService() {
-        super("MyFacebookSyncService");
+        super(MainActivity.TAG);
     }
 
     @Override
@@ -64,8 +79,38 @@ public class MyFacebookSyncService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_UPDATE_FROM_WALL.equals(action)) {
                 handleActionUpdateFromWall();
+            } else if (ACTION_POST_PHOTO.equals(action)) {
+                handlePhotoUpload(intent.getData());
             } else if (ACTION_USER_LOGOUT.equals(action)) {
                 handleActionUserLogout();
+            }
+        }
+    }
+
+    private void handlePhotoUpload(Uri photoUri) {
+        Session session = Session.getActiveSession();
+        boolean isOpened = session.isOpened();
+        Log.d(MainActivity.TAG, "Logged in to facebook: " + isOpened);
+        if (session != null && isOpened && photoUri != null) {
+            try {
+                ContentResolver resolver = getContentResolver();
+                Bitmap bitmap = BitmapFactory.decodeStream(resolver.openInputStream(photoUri));
+                Request request = Request.newUploadPhotoRequest(session, bitmap, new Request.Callback() {
+                    @Override
+                    public void onCompleted(Response response) {
+                        Log.d(MainActivity.TAG, "Response: " + response.getError());
+                    }
+                });
+                Response response = request.executeAndWait();
+                GraphObject graphObject = response.getGraphObject();
+                if (graphObject != null) {
+                    Log.d(MainActivity.TAG, graphObject.toString());
+                } else {
+                    Log.d(MainActivity.TAG, "Response: " + response);
+                }
+
+            } catch (FileNotFoundException e) {
+                Log.e(MainActivity.TAG, "Error uploading photo to Facebook!");
             }
         }
     }
@@ -78,23 +123,25 @@ public class MyFacebookSyncService extends IntentService {
         // Get active Facebook Session
         Session session = Session.getActiveSession();
         boolean isOpened = session.isOpened();
-        Log.d("MyFacebookSyncService", "Logged in to facebook: " + isOpened);
+        Log.d(MainActivity.TAG, "Logged in to facebook: " + isOpened);
         if (session != null && isOpened) {
             SharedPreferences preferences
                     = getSharedPreferences(FACEBOOK_PREFS, MODE_PRIVATE);
             long nextSinceValue = preferences.getLong(NEXT_SINCE_VALUE, -1);
-            String graphPath = "me/home?fields=id,from,message,type";
-            if(nextSinceValue > 0) {
-                graphPath += "&since=" + nextSinceValue;
+            Bundle params = new Bundle();
+            params.putString("fields", "id,from,message,type");
+            String graphPath = "me/home";
+            if (nextSinceValue > 0) {
+                params.putLong("since", nextSinceValue);
             }
-            Request request = new Request(session, graphPath);
+            Request request = new Request(session, graphPath, params, HttpMethod.GET);
             Response response = request.executeAndWait();
-            Log.d("MyFacebookSyncService", "Response: " + response.getError());
+            Log.d(MainActivity.TAG, "Response: " + response.getError());
 
             // Fetch current time to use in next request!
 
             GraphObject graphObject = response.getGraphObject();
-//            Log.d("MyFacebookSyncService", "Got graphObject: " + graphObject);
+//            Log.d(MainActivity.TAG, "Got graphObject: " + graphObject);
             if (graphObject != null) {
                 long nowInSeconds = System.currentTimeMillis() / 1000;
                 preferences.edit().putLong(NEXT_SINCE_VALUE, nowInSeconds).apply();
@@ -108,7 +155,7 @@ public class MyFacebookSyncService extends IntentService {
                         wallMessage = dataArray.getJSONObject(i);
                         storeWallMessage(wallMessage);
                     } catch (JSONException e) {
-                        Log.e("MyFacebookSyncService",
+                        Log.e(MainActivity.TAG,
                                 "Invalid message format: " + wallMessage, e);
                     }
                 }
@@ -132,13 +179,13 @@ public class MyFacebookSyncService extends IntentService {
         values.put(MyFacebookWall.Contract.MESSAGE, message);
         values.put(MyFacebookWall.Contract.TYPE, type);
         values.put(MyFacebookWall.Contract.CREATED_TIME, createdTime);
-        Uri newMessage = getContentResolver().insert(
-                MyFacebookWall.Contract.FACEBOOK_WALL_URI,
-                values);
+        Uri newMessage = getContentResolver()
+                .insert(MyFacebookWall.Contract.FACEBOOK_WALL_URI,
+                        values);
         if (newMessage == null) {
-            Log.e("MyFacebookSyncService", "Invalid message!");
+            Log.e(MainActivity.TAG, "Invalid message!");
         } else {
-            Log.d("MyFacebookSyncService", "Inserted: " + newMessage);
+            Log.d(MainActivity.TAG, "Inserted: " + newMessage);
         }
     }
 
